8d3b16d199f255c0fa36aa57cac7a191ba9a7cd0
hongjianlin1989 <ttzb1989@yahoo.com>
2021-06-29 09:19:53 -0400

    Initial Commit

---
+++ PokerApp/PokerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAppApp.swift
+//  PokerApp
+//
+//  Created by Hongjian Lin on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PokerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
